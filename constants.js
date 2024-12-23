20ae37bb956eda3a148dfd1a88d525f2913f8166
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-12-23 20:48:25 +0530

    created port listener

--- constants.js
+++ constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = 'repo-to-pdf' 
+export const PORT = 8000
---
+++ index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+import connectDB from './src/db/index.js'
+import { app } from './app.js'
+import { PORT } from './constants.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+    .then(() => {
+        app.on('error',(error) => {
+            console.log(`:: Error => ${error} ::`);
+            throw error
+        })
+        app.listen(PORT || 8001, () => {
+            console.log(`:: Server is running at port ${PORT} or check port 8001 ::`);
+        })
+    })
+    .catch((error) => {
+        console.log(`:: Mongo DB connection failed, error => ${error} ::`);
+    })
